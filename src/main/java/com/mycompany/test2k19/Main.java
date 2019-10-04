d1de619a56f477309b3847092b5ddb4ad8fa7ad4
Prynerd <atti13@gmail.hu>
2019-10-04 13:57:47 +0200

    pom.xml fix & application properties

--- src/main/java/com/mycompany/test2k19/Main.java
+++ src/main/java/com/mycompany/test2k19/Main.java
@@ -1,16 +1,20 @@
 package com.mycompany.test2k19;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  *
  * @author Prynerd
  */
+@SpringBootApplication
+@ComponentScan("com.mycompany.test2k19")
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
+        SpringApplication.run(Main.class, args);
         
     }
     
