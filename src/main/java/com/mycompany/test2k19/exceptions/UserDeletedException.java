97fd6563d8402a88d76ca497a76c9d329df314e0
Urban Attila <attilaurban94@gmail.com>
2019-10-11 14:56:37 +0200

    UserDeletedException

---
+++ src/main/java/com/mycompany/test2k19/exceptions/UserDeletedException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.test2k19.exceptions;
+
+/**
+ *
+ * @author Pry
+ */
+public class UserDeletedException extends RuntimeException{
+
+    public UserDeletedException(String message) {
+        super(message);
+    }
+    
+    
+    
+}
