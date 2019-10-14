7f27d26b91c66a2d7e62dcc1137dc66cb714339e
Urban Attila <attilaurban94@gmail.com>
2019-10-14 14:59:25 +0200

    ExistsException

---
+++ src/main/java/com/mycompany/test2k19/exceptions/ExistsException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.test2k19.exceptions;
+
+/**
+ *
+ * @author Pry
+ */
+public class ExistsException extends RuntimeException{
+
+    public ExistsException(String message) {
+        super(message);
+    }
+    
+}
